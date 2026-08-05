180971965177e51cef6410bd94acaa9dc17ccbac
Lim Jun Jie <jjlim9012@gmail.com>
2026-08-06 01:08:58 +0800

    Delete src directory

--- src/main/scala/model/Errors.scala
+++
@@ -1,8 +0,0 @@
-package model
-
-sealed trait AppError:
-  def message: String
-
-case class ValidationError(override val message: String) extends AppError
-case class NotFoundError(override val message: String) extends AppError
-case class PersistenceError(override val message: String) extends AppError
